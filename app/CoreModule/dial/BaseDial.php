cbbb7aa49dc67b676d06d7d9fae87b2352fa17a6
Tomáš Vodička <vodictomas@gmail.com>
2026-09-23 19:10:50 +0200

    phpstan

--- app/CoreModule/dial/BaseDial.php
+++ app/CoreModule/dial/BaseDial.php
@@ -6,12 +6,17 @@ namespace Core\Dial;
 
 abstract class BaseDial
 {
+	/** @return array<string, string> */
+	abstract protected static function translate(): array;
+
+
+	/** @return array<string, string> */
 	public static function getList(): array
 	{
 		return static::translate();
 	}
 
-	public static function getString(string $value)
+	public static function getString(string $value): string
 	{
 		return static::translate()[$value];
 	}
